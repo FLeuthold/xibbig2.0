63d6ec5571816a08006afd5cd7fdd59b2889ac34
Fabio Leuthold <fabio.leuthold@hotmail.ch>
2023-05-06 17:34:00 +0200

    initial commit

---
+++ skript.js
@@ -0,0 +1,63 @@
+let rotateAngle = 90;
+let left = 90;
+let topp = 90;
+let zehntner = 1;
+let infraX = 0;
+let infraDX = 5;
+
+infra.setAttribute("style","zIndex: -30 ")
+setInterval(mooveit,40, infra);
+for (let i = 0; i < 5; i++) {
+  setTimeout(addSmartlearn, i * 1000, i);
+}
+setTimeout(jumpScare, 11*1000)
+
+
+
+
+
+function mooveit(infra){
+      if (infraX > document.documentElement.offsetWidth - infra.clientWidth) {
+        infraDX= - 5
+      }
+      if (infraX < -1) {
+        infraDX= 5
+      }
+      infraX = infraX+infraDX;
+      infra.setAttribute("style","zIndex: -30;left:"+infraX+"px;")
+}
+
+function addSmartlearn(i){
+  let dymimg = new Image();
+  dymimg.src = "img/smartlearn_alpha.png";
+  let gb = document.getElementById("gibbixbehaelter");
+  gb.appendChild(dymimg);
+  let inti=  setInterval(rotate,100, dymimg, i);
+}
+
+function jumpScare(){
+  const animation = new Image();
+  animation.src = "img/explosion.gif";
+  animation.style.height = '500px';
+  animation.style.top = '200px'
+  animation.style.zIndex = '20';
+  let gb = document.getElementById("gibbixbehaelter");
+  gb.appendChild(animation);
+  let autio = document.getElementById("kekw")
+  autio.autoplay="true";
+  autio.play();
+}
+
+function rotate(image, z) {
+  
+  image.setAttribute("style", "transform: rotate(" + rotateAngle + "deg);"
+  + "top: " + topp+ "px;"+"left: " + left+ "px;"+ "width:300px;"
+  + "z-index:" + z);
+  
+  const getRandom = max => {
+      return Math.floor(Math.random() * max) - max / 2;
+  };
+  rotateAngle = rotateAngle + getRandom(50);
+  topp = getRandom(100)+200;
+  left = getRandom(100)+500;
+}
